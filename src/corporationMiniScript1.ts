b3527b663e9eb50568e12d3cefc44517f456e643
CatLover <152669316+catloversg@users.noreply.github.com>
2024-03-20 10:14:11 +0700

    Minor update

--- src/corporationMiniScript1.ts
+++ src/corporationMiniScript1.ts
@@ -38,11 +38,11 @@ export async function main(ns: NS): Promise<void> {
         let finish = true;
         for (const city of cities) {
             const office = ns.corporation.getOffice(divisionName, city);
-            if (office.avgEnergy < 98) {
+            if (office.avgEnergy < 99.5) {
                 ns.corporation.buyTea(divisionName, city);
                 finish = false;
             }
-            if (office.avgMorale < 98) {
+            if (office.avgMorale < 99.5) {
                 ns.corporation.throwParty(divisionName, city, 500000);
                 finish = false;
             }
